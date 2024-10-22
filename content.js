7764b0714132798604cc0e3914f1c7dcd2361d0b
Sangdon1029 <gorist@likelion.org>
2024-10-22 10:27:00 +0900

    feat: 포트폴리오 슬랙채널에 연동

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.action === "fetchElementData") {
+  if (message.action === "sendToSlack" || message.action === "copy") {
     // id가 portfolioModal인 요소를 가져옴
     const element = document.getElementById('portfolioModal');
     
     if (element) {
       // 요소의 dataset에서 필요한 data-* 속성들을 추출
+      const isTest = message.isTestServer;  // isTestServer 속성 값
+      const urlPrefix = message.prefix;  // prefix 속성 값
       const dataExpertName = element?.dataset?.expertname;  // data-expert-name 속성 값
       const dataCode = element.dataset.code;  // data-code 속성 값
-      const prefix = 'https://cdn-optimized.imweb.me/';
-      const dataUrl = `${prefix}${element.dataset.url}`;    // data-url 속성 값
+      const dataUrl = `${message.prefix}/${element.dataset.url}`;    // data-url 속성 값
       const dataTitle = element.dataset.title;  // data-title 속성 값
       const dataDomain = element.dataset.domain; // data-domain 속성 값
+      const portfolioHost = isTest ? "https://imtest.me" : "https://imweb.me";
+      const portfolioLink = `${portfolioHost}/expert/portfolio-detail/${dataCode}`;  // 포트폴리
+    
       
       // 데이터를 객체로 만들어 보냄
       const elementData = {
@@ -18,11 +22,12 @@ chrome.runtime.onMessage.addListener((message) => {
         url: dataUrl,
         title: dataTitle,
         domain: dataDomain,
-        expertName: dataExpertName
+        expertName: dataExpertName,
+        portfolioLink: portfolioLink
       };
 
       // 추출한 데이터를 popup.js나 다른 스크립트로 전달
-      chrome.runtime.sendMessage({action: "updateElementData", data: elementData});
+      chrome.runtime.sendMessage({action: message.action, data: elementData});
     } else {
       console.log("Element with ID 'portfolioModal' not found.");
       chrome.runtime.sendMessage({action: "updateElementData", data: 'Element not found'});
--- popup.js
+++ popup.js
@@ -1,30 +1,108 @@
+const slackEndPoint = "https://hooks.slack.com/services/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";
+const sendMessageToSlackWithFile = (message, fileUrl) => {
+  fetch(slackEndPoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      text: message,
+      attachments: [
+        {
+          imageUrl: fileUrl,
+          thumb_url: fileUrl,
+          color: "#36a64f",
+          fields: [
+            {
+              imageUrl: fileUrl,
+              short: false
+            }
+          ]
+        }
+      ]
+    })
+  }).then((response) => {
+    console.log("Message sent to Slack: ", message);
+    console.log("File URL: ", fileUrl);
+  }).catch((error) => {
+    console.error("Error sending message to Slack: ", error);
+  });
+}
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text).then(() => {
+    document.getElementById('result').innerText = "복사하였습니다.";
+    console.log("Markdown data copied to clipboard: ", text);
+  }).catch(err => {
+    document.getElementById('result').innerText = "뭔가 잘못되었습니다.";
+    console.log("Markdown data copied to clipboard: ", text);
+  });
+}
+const getTabUrl = () =>{
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    const tabUrl = tabs[0].url;
+    const url = new URL(tabUrl);
+    const domain = url.hostname;
+    return domain;
+  });
+}
+
+/**
+ * @param {*} url 
+ * @returns  
+ * - production: imweb.me
+ * - development: localhost / imtest.me
+ */
+const detectHost = (url) => {
+  if(String(url).includes("imweb")){
+    return "production";
+  } else {
+    return "development";
+  }
+}
 document.getElementById('btn').addEventListener('click', function() {
+  const domain = getTabUrl();
+  const isTestServer = detectHost(domain) === "development";
+  const testServerCdnUrl = "https://cdn-test.imtest.me";
+  const productionCdnUrl = "https://cdn-optimized.imweb.me";
+  const prefix = isTestServer ? testServerCdnUrl : productionCdnUrl;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       // 현재 활성화된 탭에 메시지 전송
-      chrome.tabs.sendMessage(tabs[0].id, {action: "fetchElementData"});
+      chrome.tabs.sendMessage(tabs[0].id, {action: "copy", prefix: prefix, isTestServer: isTestServer});
+  });
+});
+
+document.getElementById('btn_2').addEventListener('click', function() {
+  const domain = getTabUrl();
+  const isTestServer = detectHost(domain) === "development";
+  const testServerCdnUrl = "https://cdn-test.imtest.me";
+  const productionCdnUrl = "https://cdn-optimized.imweb.me";
+  const prefix = isTestServer ? testServerCdnUrl : productionCdnUrl;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      // 현재 활성화된 탭에 메시지 전송
+      chrome.tabs.sendMessage(tabs[0].id, {action: "sendToSlack", prefix: prefix, isTestServer: isTestServer});
   });
 });
 
 // 데이터를 받아서 팝업에 출력하는 로직 (선택 사항)
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.action === "updateElementData") {
-    // 클립보드에 복사하기 
+  
+  if(message.data.title === undefined){
+    document.getElementById('result').innerText = "포트폴리오를 선택해주세요";
+    alert("포트폴리오를 선택해주세요");
+    return null
+  } 
 
-    const markdownText = `
-    ${message.data.domain}
-    - ${message.data.title}
-    - ${message.data.expertName}
-    - **[${message.data.code}](https://imweb.me/expert/portfolio-detail/${message.data.code})**
-    - **imageUrl: [${message.data.url}](${message.data.url})**
-    `.trim();  // 불필요한 공백 제거
-        
-    navigator.clipboard.writeText(markdownText).then(() => {
-      document.getElementById('result').innerText = "copy to clipboard";
-      console.log("Markdown data copied to clipboard: ", markdownText);
-    }).catch(err => {
-      document.getElementById('result').innerText = "something wrong";
-      console.log("Markdown data copied to clipboard: ", markdownText);
-    });
+  const markdownText = `
+  ${message.data.domain}
+  - ${message.data.title}
+  - ${message.data.expertName}
+  - portfolio_link : ${message.data.portfolioLink}
+  - imageUrl: ${message.data.url}
+  `.trim(); 
+
+  // 클립보드에 복사하기 
+  if(message.action === "copy"){
+    copyToClipboard(markdownText);
+  }
+  if (message.action === "sendToSlack") {
+    sendMessageToSlackWithFile(markdownText, message.data.url);
   }
 });
 
